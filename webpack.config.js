a72b774add2d6512b059dbcc51c9e0fc7b04fe79
aochujie <aochujie>
2017-12-01 17:09:57 +0800

    add build script

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports={
     output:{
         path: path.resolve(__dirname, './build'),
         filename:'bundle.js',
-        publicPath: '/static/'
+        publicPath: '/statics/'
     },
     module:{
         loaders:[
